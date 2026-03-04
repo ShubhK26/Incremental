2952a4de451995983acdaadc1db1f703ce3cb5b0
Vaibhavipatil09 <dummy@gmail.com>
2026-03-04 09:46:12 +0000

    day 12 commit

--- server/src/main/java/com/edutech/progressive/config/SecurityConfig.java
+++ server/src/main/java/com/edutech/progressive/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.edutech.progressive.config;
 
-// import com.edutech.progressive.jwt.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,56 +15,58 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.edutech.progressive.jwt.JwtRequestFilter;
+
 @Configuration
 @EnableWebSecurity
-// @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    // private final UserDetailsService userDetailsService;
-    // private final JwtRequestFilter jwtRequestFilter;
-    // private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
+    private final PasswordEncoder passwordEncoder;
 
-    // @Autowired
-    // public SecurityConfig(UserDetailsService userDetailsService,
-    //         JwtRequestFilter jwtRequestFilter,
-    //         PasswordEncoder passwordEncoder) {
-    //     this.userDetailsService = userDetailsService;
-    //     this.jwtRequestFilter = jwtRequestFilter;
-    //     this.passwordEncoder = passwordEncoder;
-    // }
+    @Autowired
+    public SecurityConfig(UserDetailsService userDetailsService,
+            JwtRequestFilter jwtRequestFilter,
+            PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.jwtRequestFilter = jwtRequestFilter;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-    // @Override
-    // protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    //     auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    // }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-        // .cors().and().csrf().disable()
+        .cors().and().csrf().disable()
                 .authorizeRequests()
-                // .antMatchers("/user/register", "/user/login").permitAll()
-                // .antMatchers(HttpMethod.GET, "/student/**").hasAnyAuthority("STUDENT", "TEACHER")
-                // .antMatchers(HttpMethod.POST, "/student/**").hasAuthority("STUDENT")
-                // .antMatchers(HttpMethod.PUT, "/student/**").hasAuthority("STUDENT")
-                // .antMatchers(HttpMethod.DELETE, "/student/**").hasAuthority("STUDENT")
-                // .antMatchers(HttpMethod.GET, "/course/**").hasAnyAuthority("STUDENT", "TEACHER")
-                // .antMatchers(HttpMethod.POST, "/course/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.PUT, "/course/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.DELETE, "/course/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.GET, "/teacher/**").hasAnyAuthority("STUDENT", "TEACHER")
-                // .antMatchers(HttpMethod.POST, "/teacher/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.PUT, "/teacher/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.DELETE, "/teacher/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.GET, "/attendance/**").hasAnyAuthority("STUDENT", "TEACHER")
-                // .antMatchers(HttpMethod.POST, "/attendance/**").hasAuthority("TEACHER")
-                // .antMatchers(HttpMethod.DELETE, "/attendance/**").hasAuthority("TEACHER")
-                // .antMatchers("/enrollment/**").hasAnyAuthority("STUDENT", "TEACHER")
+                .antMatchers("/user/register", "/user/login").permitAll()
+                .antMatchers(HttpMethod.GET, "/student/**").hasAnyAuthority("STUDENT", "TEACHER")
+                .antMatchers(HttpMethod.POST, "/student/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.PUT, "/student/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.DELETE, "/student/**").hasAuthority("STUDENT")
+                .antMatchers(HttpMethod.GET, "/course/**").hasAnyAuthority("STUDENT", "TEACHER")
+                .antMatchers(HttpMethod.POST, "/course/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.PUT, "/course/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.DELETE, "/course/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.GET, "/teacher/**").hasAnyAuthority("STUDENT", "TEACHER")
+                .antMatchers(HttpMethod.POST, "/teacher/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.PUT, "/teacher/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.DELETE, "/teacher/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.GET, "/attendance/**").hasAnyAuthority("STUDENT", "TEACHER")
+                .antMatchers(HttpMethod.POST, "/attendance/**").hasAuthority("TEACHER")
+                .antMatchers(HttpMethod.DELETE, "/attendance/**").hasAuthority("TEACHER")
+                .antMatchers("/enrollment/**").hasAnyAuthority("STUDENT", "TEACHER")
                 .anyRequest().permitAll()
                 .and()
-                .csrf().disable();
-                // .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
--- server/src/main/java/com/edutech/progressive/dto/LoginRequest.java
+++ server/src/main/java/com/edutech/progressive/dto/LoginRequest.java
@@ -1,22 +1,22 @@
-// package com.edutech.progressive.dto;
+package com.edutech.progressive.dto;
 
-// public class LoginRequest {
-//    private String username;
-//    private String password;
+public class LoginRequest {
+   private String username;
+   private String password;
 
-//    public String getUsername() {
-//       return username;
-//    }
+   public String getUsername() {
+      return username;
+   }
 
-//    public void setUsername(String username) {
-//       this.username = username;
-//    }
+   public void setUsername(String username) {
+      this.username = username;
+   }
 
-//    public String getPassword() {
-//       return password;
-//    }
+   public String getPassword() {
+      return password;
+   }
 
-//    public void setPassword(String password) {
-//       this.password = password;
-//    }
-// }
+   public void setPassword(String password) {
+      this.password = password;
+   }
+}
