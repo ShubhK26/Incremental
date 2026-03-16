196d18368db290fe8dc22c9c756c9ccf5b11ac6f
ShubhK26 <dummy@gmail.com>
2026-03-16 12:36:49 +0000

    day 22 final

--- client/src/app/educonnect/models/User.ts
+++ client/src/app/educonnect/models/User.ts
@@ -1,20 +1,20 @@
-// import { Student } from "./Student";
-// import { Teacher } from "./Teacher";
+import { Student } from "./Student";
+import { Teacher } from "./Teacher";
 
 export class User {
     userId: number;
     username: string;
     password: string;
     role: string;
-    studentId?: number;
-    teacherId?: number;
+    student?: Student | undefined;
+    teacher?: Teacher | undefined;
   
-    constructor(userId: number, username: string, password: string, role: string, studentId?: number, teacherId?: number) {
+    constructor(userId: number, username: string, password: string, role: string, student: Student | undefined, teacher: Teacher | undefined) {
       this.userId = userId;
       this.username = username;
       this.password = password;
       this.role = role;
-      this.studentId = studentId;
-      this.teacherId = teacherId;
+      this.student = student;
+      this.teacher = teacher;
     }
   }
